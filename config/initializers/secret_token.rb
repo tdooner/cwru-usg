01bfc664cee3f8584bca3d5eaf2584405aaeae30
Tom Dooner <tomdooner@gmail.com>
2012-01-09 10:31:39 -0500

    Initial Rails App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CwruUsg::Application.config.secret_token = '0f6bba2e475f12464a7209f94c2b91bb192c03aed7018f3d122fdb001e62502713716d8d77ede05b8602c2060d130cf19ffee0afffff505a74da81960994ab6b'
